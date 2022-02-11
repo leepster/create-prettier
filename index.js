9f5a83614106f29fa90db6927f2c16ab50cdfcc4
Tony Leeper <tonyleeper@blueyonder.co.uk>
2022-02-11 18:04:24 +0000

    creates a .prettierrc file with basic settings

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
+const fs = require('fs')
+const path = require('path')
 
 function main() {
-  console.log('Hello from main')
+  fs.writeFileSync(
+    path.join(__dirname, '.prettierrc'),
+    `{
+  "trailingComma": "es5",
+  "tabWidth": 2,
+  "semi": false,
+  "singleQuote": true,
+  "printWidth": 120
+}`
+  )
+
+  console.log('.prettierrc')
 }
 
 if (require.main === module) {
