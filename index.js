c05a68eb68b717809f8d0d110e9b9e184be8a9ff
Tony Leeper <tonyleeper@blueyonder.co.uk>
2022-02-12 18:41:42 +0000

    changed from fs to fs/promises

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
-import fs from 'fs'
+import fs from 'fs/promises'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import chalk from 'chalk'
 
-const source = path.join(path.dirname(fileURLToPath(import.meta.url)), './template')
-fs.cpSync(source, process.cwd(), { recursive: true })
-console.info(chalk.greenBright('created:'), chalk.cyanBright('.prettierrc'))
-console.info(chalk.greenBright('created:'), chalk.cyanBright('.idea/pretter.xml'))
+async function main() {
+  const source = path.join(path.dirname(fileURLToPath(import.meta.url)), './template')
+  await fs.cp(source, process.cwd(), { recursive: true })
+  console.info(chalk.greenBright('created:'), chalk.cyanBright('.prettierrc'))
+  console.info(chalk.greenBright('created:'), chalk.cyanBright('.idea/pretter.xml'))
+}
+
+main().catch((error) => console.error(error))
